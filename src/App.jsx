d52a03fdfcb11ca04b072b1e81ce22e44b647fa5
PrasanthVadde <prasanthvadde7@gmail.com>
2024-08-23 22:36:01 +0530

    modified the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
-// import NavBar from "./react-bootstrap/navbar";
-// import DefaultCard from "./react-bootstrap/card";
-// import DefaultCarousel from "./react-bootstrap/carousels";
-// import Footer from "./react-bootstrap/footer";
-// import { InstagramLoader } from "./components/contentLoader/instagramContentLoader";
-// import { GitIcon } from "./components/icons/dev-icons";
+
 import { Recipe } from "./components/recipeListing/recipeList";
 function App() {
   return (
     <>
     <Recipe/>
-      {/* <NavBar />
-      <DefaultCarousel />
-      <div style= {{display:"flex", flexWrap:"wrap", justifyContent:"space-evenly",padding:"4px"}}>
-      <DefaultCard/>
-      <DefaultCard/>
-      <DefaultCard/>
-      <DefaultCard/>
-      </div>
-      <GitIcon/>
-      <InstagramLoader/>
-      <Footer/> */}
+     
     </>
   );
 }
