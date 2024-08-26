51238eede0df24709eace55e4ad2a6967c14331e
PrasanthVadde <prasanthvadde7@gmail.com>
2024-08-26 11:22:07 +0530

    updated the app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import NavBar from "./react-bootstrap/navbar";
 import DefaultCarousel from "./react-bootstrap/carousels";
 import Footer from "./react-bootstrap/footer";
-import { ClassComponent } from "./components/class-component/classComponent";
-import { Button } from "./components/class-component/youtubeButton";
+import{ClassComponent} from "./components/class-component/classComponent.jsx"
 
 function App() {
   return (
     <>
-      <Button />
       <NavBar />
       <DefaultCarousel />
       <ClassComponent/>
