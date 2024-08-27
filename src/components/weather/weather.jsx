4915487708793a6f37fbd31987269c13d5b79648
PrasanthVadde <prasanthvadde7@gmail.com>
2024-08-27 15:42:27 +0530

    added the themes based on the temperature

--- src/components/weather/weather.jsx
+++ src/components/weather/weather.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { Component } from "react";
 import { CustomSpinner } from "../spinners/tailspin";
+import Card from "react-bootstrap/Card";
 
 export class Weather extends Component {
   state = {
@@ -28,6 +29,16 @@ export class Weather extends Component {
       console.error("Error");
     }
   }
+  getBackgroundImage = () => {
+    const temp = this.state.result.temp_max;
+    if (Math.floor(temp - 273.15) < 0) {
+      return "url('https://media.istockphoto.com/id/637409946/photo/thermometer-on-snow-shows-low-temperatures-under-zero.jpg?s=612x612&w=0&k=20&c=fmRJtO3RRIMA6TV3JI93CSlteBTrQI1PAjmWaRLiBlA=')";
+    } else if (Math.floor(temp - 273.15) >= 0 && Math.floor(temp - 273.15) < 25) {
+      return "url('https://media.istockphoto.com/id/1501165712/photo/the-evening-sky-is-filled-with-the-creative-and-dusky-cirrus-clouds.jpg?s=612x612&w=0&k=20&c=0NiKxj0FFj0n8ydsOg3aQaaxXyVwD-GrdeXlyw8oW9w=')";
+    } else {
+      return "url('https://media.istockphoto.com/id/1323823418/photo/low-angle-view-thermometer-on-blue-sky-with-sun-shining.jpg?s=612x612&w=0&k=20&c=LwLCGF902C-DNwKgCMCR12zFnB4g1INWzlk1JPOidRk=')";
+    }
+  };
 
   fetchData = async (lat, long) => {
     const { data } = await axios.get(
@@ -51,11 +62,36 @@ export class Weather extends Component {
           <CustomSpinner />
         ) : (
           <>
-            <h3>Longitude: {lon}</h3>
-            <h3>Latitude: {lat}</h3>
-            <h3>Max-Temperature: {Math.floor(temp_max - 273.15)}° Celsius</h3>
-            <h3>Min-Temperature: {Math.floor(temp_min - 273.15)}° Celsius</h3>
-            <h3>Location:{name}</h3>
+            <div
+              style={{
+                display: "flex",
+                height: "100vh",
+                backgroundImage: this.getBackgroundImage(),
+                filter: "brightness(50%)",
+                backgroundSize: "cover",
+              }}
+            >
+              <Card
+                style={{
+                  width: "18rem",
+                  backgroundColor:"transparent",
+                  border:"none",
+                  color:"white"
+                }}
+              >
+                <Card.Body>
+                  <Card.Text>Longitude: {lon}</Card.Text>
+                  <Card.Text>Latitude: {lat}</Card.Text>
+                  <Card.Text>Location:{name}</Card.Text>
+                  <Card.Text>
+                    Max-Temperature: {Math.floor(temp_max - 273.15)}° C
+                  </Card.Text>
+                  <Card.Text>
+                    Min-Temperature: {Math.floor(temp_min - 273.15)}° C
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
           </>
         )}
       </>
